a45f248eae5acd518e5c028bd4e64b3936d43936
SarahDal <dalrymple.sarah@gmail.com>
2024-05-05 17:44:54 +0100

    publish site from docs dir

---
+++ docs/assets/js/lunr/lunr-store.js
@@ -0,0 +1,79 @@
+var store = [{
+        "title": "New York to San Fransisco on the California Zephyr",
+        "excerpt":"Years ago, for some reason, I decided to take a train from Edinburgh to Istanbul for my brother’s wedding. Back in the near-mystical ancient times of 2010, you could only book trains in Western Europe through the DB website. Now, I could book the whole way in a few clicks,...","categories": ["journeys"],
+        "tags": ["trains","holday","journey","old_blog"],
+        "url": "/journeys/2017/03/01/NY2SF.html",
+        "teaser": "/www/assets/images/NYSF/20170330204507-0bf5b0eb-sm.jpg"
+      },{
+        "title": "Intro",
+        "excerpt":"I’m a coastal ecologist specialising in seabirds (particularly large gulls, like Herring and Lesser Black-backed gulls) with an interest in tech, small board computers, coding, drones, and GIS. I’ve had an online presence since the 1990s, and as current social media seems to be dragging everything along, I’ve been enjoying...","categories": [],
+        "tags": [],
+        "url": "/2023/12/16/intro.html",
+        "teaser": null
+      },{
+        "title": "Home Assistant",
+        "excerpt":"Home Assistant runs on a Raspberry Pi and can control most modern Smart Home devices, plus a whole load more you can make yourself. I started off using ESPHome to control ESP32s with simple temperature and humidity monitors. They send their data using MQTT over bluetooth. From there, I hacked...","categories": ["Home Assistant"],
+        "tags": [],
+        "url": "/home%20assistant/2023/12/17/HomeAssistant.html",
+        "teaser": null
+      },{
+        "title": "Arduino Car Counter",
+        "excerpt":"While talking about electronics at work, my colleague mentioned a car counter he’d bought for a large sum of money that never really worked. I had the idea that I could build one myself, from Arduino, and thought I’d give it a try. In 2019 when I did this project,...","categories": ["Arduino-ESP32"],
+        "tags": [],
+        "url": "/arduino-esp32/2023/12/17/carcounter.html",
+        "teaser": "/www/assets/images/CC_housing.jpg"
+      },{
+        "title": "Has the cat been fed?",
+        "excerpt":"If, like me, you have a cat, you will be aware that they’re very good at persuading you that they’ve not had breakfast yet, even though you could swear you just fed them 10 minutes ago. To limit overfeeding, I use Home Assistant to log when mine’s been fed, and...","categories": ["Home Assistant"],
+        "tags": [],
+        "url": "/home%20assistant/2023/12/18/cat-fed.html",
+        "teaser": null
+      },{
+        "title": "Blawearie",
+        "excerpt":"Up on the desolate Bewick Moor in Northumberland stands a windswept and isolated ruin of the shepherd’s house, Blawearie. Now that it’s surrounded by land intensively managed for pheasant and partridge, it feels a little less remote than when I first visited it 20 years ago, but it’s still a...","categories": ["Northumberland"],
+        "tags": ["geneology","northumberland","history"],
+        "url": "/northumberland/2024/01/08/blaewearie.html",
+        "teaser": "/www/assets/images/blae/Blawearie.jpg"
+      },{
+        "title": "Coquetdale Whisky",
+        "excerpt":"In upper Coquetdale in the remotest part of Northumberland, evidence still remains of illicit whisky-distilling in the 1700s, supplying illegal drinking dens. Cut off from excise men by the sheer remoteness of the area, shepherds and farmers, as well as cattle-drovers and smugglers heading down from Scotland, drank at unlicenced...","categories": ["Northumberland"],
+        "tags": ["walks","northumberland","history"],
+        "url": "/northumberland/2024/01/27/WhiskyStills.html",
+        "teaser": "/www/assets/images/whisky/LowerMidhope.jpg"
+      },{
+        "title": "Blindburn Still",
+        "excerpt":"Back into Coquetdale to find the Blindburn or Beefstand Still. Very wet underfoot as I followed the Blindburn from the farmhouse up to find moss-covered remains of this still, said to be one belonging to an infamous smuggler called Black Rory. You can make out where the still would have...","categories": ["Northumberland"],
+        "tags": ["walks","northumberland","history"],
+        "url": "/northumberland/2024/02/17/BlindburnStill.html",
+        "teaser": "/assets/images/blindburn/BuckhamsWallsBurn.jpg"
+      },{
+        "title": "Rory's Still",
+        "excerpt":"A fine morning to walk out again in Upper Coquetdale and find the last of the extant illicit stills, this time Rory’s Still by the Usway Burn. It was still frosty as I walked out from the car park at Rowhope, following a track along the Trows Burn, past Murder...","categories": ["Northumberland"],
+        "tags": ["walks","northumberland","history"],
+        "url": "/northumberland/2024/02/24/RorysStill.html",
+        "teaser": "/assets/images/usway/Rowhope.jpg"
+      },{
+        "title": "Around Edlingham",
+        "excerpt":"We went out today to look at a handful of neat historical things in the hills and woods around Edlingham, in Northumberland. Turning off the A697 at what was once the Swinburne Arms on the old turnpike, the B6341 cuts above the village of Edlingham on its way to Alnwick....","categories": ["Northumberland"],
+        "tags": ["walks","northumberland","history"],
+        "url": "/northumberland/2024/03/15/Corbys.html",
+        "teaser": "/assets/images/image.jpg"
+      },{
+        "title": "Williams Cleuch Pines",
+        "excerpt":"I drove across Redesdale and the Wannies today to Kielder Forest, to look at a handful of very special trees.. Kielder Forest was planted up between the wars, to provide a strategic timber resource for the nation. In the late 1970s the valley was flooded, to provide water for a...","categories": ["Northumberland"],
+        "tags": ["walks","northumberland","history"],
+        "url": "/northumberland/2024/04/07/WilliamsCleuchPines.html",
+        "teaser": "/assets/images/image.jpg"
+      },{
+        "title": "Redesdale Border Walk",
+        "excerpt":"This weekend I took a walk high above Redesdale, an area notorious for lawless border reivers up to the 1600s. My walk took me up through the Northern part of the Redesdale forest, past Spithope Bothy. This simple stone shelter, surrounded by a large walled area, was probably once a...","categories": ["Northumberland"],
+        "tags": ["walks","northumberland","history"],
+        "url": "/northumberland/2024/04/21/Whitelee.html",
+        "teaser": "/assets/images/image.jpg"
+      },{
+        "title": "Merrick and the Galloway Hills",
+        "excerpt":"Across the border far into the Galloway Hills this weekend. I did walk this area about 12 years ago and loved it, so a return trip. From the very end of the Glentrool road I parked at Bruce’s Stone car park, then followed the lovely Buchan Burn past the Culsharg...","categories": ["walks"],
+        "tags": ["hiking"],
+        "url": "/walks/2024/05/04/Merrick.html",
+        "teaser": "/assets/images/images/Merrick/Neive.jpg"
+      }]
